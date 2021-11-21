3dc51599b0eabbef330d89f0d16bb4fd9690943e
kenny <kcheng16@gmail.com>
2021-11-20 20:12:04 -0800

    created listings reducer

---
+++ frontend/actions/listing_actions.js
@@ -0,0 +1,47 @@
+import * as listingApiUtil from '../util/listings_util'
+
+export const RECEIVE_LISTINGS = 'RECEIVE_LISTINGS'
+export const RECEIVE_LISTING = 'RECEIVE_LISTING'
+export const REMOVE_LISTING = 'REMOVE_LISTING'
+//=================================================
+
+export const receiveListings = (listings) => ({
+  type: RECEIVE_LISTINGS,
+  listings
+})
+
+export const receiveListing = (listing) => ({
+  type: RECEIVE_LISTING,
+  listing
+})
+
+export const removeListing = (listingId) => ({
+  type: REMOVE_LISTING,
+  listingId
+})
+//=================================================
+
+export const requestListings = () => dispatch => (
+  listingApiUtil.fetchListings()
+    .then(listings => dispatch(receiveListings(listings)))
+)
+
+export const requestListing = (listingId) => dispatch => (
+  listingApiUtil.fetchListing(listingId)
+    .then(listing => dispatch(receiveListing(listing)))
+)
+
+export const createListing = (listing) => dispatch => (
+  listingApiUtil.createListing(listing)
+    .then(listing => dispatch(receiveListing(listing)))
+)
+
+export const updateListing = (listing) => dispatch(
+  listingApiUtil.updateListing(listing)
+    .then(listing => dispatch(receiveListing(listing)))
+)
+
+export const deleteListing = (listingId) => dispatch (
+  listingApiUtil.deleteListing(listingId)
+    .then(() => dispatch(removeListing(listingId)))
+)
--- frontend/util/listings_util.jsx
+++ frontend/util/listings_util.jsx
@@ -11,3 +11,26 @@ export const fetchListing = (listingId) => (
     url: `/api/listings/${listingId}`
   })
 )
+
+export const createListing = (listing) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/listings`,
+    data: {listing}
+  })
+)
+
+export const updateListing = (listing) => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/listings/${listing.id}`,
+    data: {listing}
+  })
+)
+
+export const deleteListing = (listingId) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/listings/${listingId}`
+  })
+)
