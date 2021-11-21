89a7541922659fcd78e3ae148580a8e3c0e33d52
kenny <kcheng16@gmail.com>
2021-11-20 16:20:26 -0800

    changed routes back to original MVP; added Listing controller

---
+++ app/controllers/api/listings_controller.rb
@@ -0,0 +1,69 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id             :bigint           not null, primary key
+#  cat_friendly   :boolean          not null
+#  city           :string           not null
+#  country        :string           not null
+#  description    :text             not null
+#  latitude       :float            not null
+#  longitude      :float            not null
+#  num_of_beds    :integer          not null
+#  postal_code    :integer          not null
+#  price          :integer          not null
+#  street_address :string           not null
+#  title          :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  host_id        :integer          not null
+#
+# Indexes
+#
+#  index_listings_on_host_id         (host_id) UNIQUE
+#  index_listings_on_street_address  (street_address) UNIQUE
+#
+class ListingsController < ApplicationController
+  def index
+    
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      render :show
+    else
+      render json: @listing.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find_by(id: params[:id])
+    
+    if @listing.update(listing_params)
+      render 'api/listings/show'
+    else
+      render json: @listing.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+
+    if @listing.destroy
+      render 'api/listings' #not sure since user must be logged: user/listings??
+    else
+      render json: @listing.errors.full_messages, status: 422
+    end
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :street_address, :city, :postal_code, :country, :longitude, :latitude, :price, :num_of_beds, :cat_friendly)
+    #not included: host_id
+  end
+end
