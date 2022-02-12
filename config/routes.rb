f1751f46dfb047d0c9700eca23760226dc3fab5d
kenny <kcheng16@gmail.com>
2022-02-12 13:33:16 -0800

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
-    
-    # resources :users, only: [:create, :show] do
-    #   resources :reservations, only: [:index, :create, :destroy, :update]
-    #   resources :reviews, only: [:index, :create, :destroy, :update]
-    #   resources :listings, only: [:index, :show, :create, :destroy, :update]
-    # end
-    # resources :listings, only: [:index, :show]
-
     resources :users, only: [:create, :show]
     resources :reservations, only: [:show, :create, :destroy, :update]
     resources :reviews, only: [:create, :destroy, :update]
     resources :listings, only: [:index, :show, :create, :destroy, :update]
-
-    #should not need to nest ':create', since we CAN block users from viewing
-    # the create page IF they're not logged in
+    #should not need to nest ':create', since we CAN block users from viewing the create page IF they're not logged in
+    
   end
   
   root to: 'static_pages#root'
